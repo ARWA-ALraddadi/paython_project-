086bf4384f6e635a76ffb4a5ef11d5f04eaca68d
ARWA <52074173+ARWA-ALraddadi@users.noreply.github.com>
2021-08-05 23:48:51 +1000

    Create 1_three_circles.py

---
+++ 02-Workshop/Workshop-Questions/1_three_circles.py
@@ -0,0 +1,52 @@
+# Three non-overlapping circles
+#
+# As a simple exercise in using the Turtle package you are
+# required to draw three circles on the screen.  Each circle
+# must be of a different size and colour.  Most importantly,
+# the circles must not touch or overlap at any point, nor
+# can one circle appear inside another.
+#
+# NB: We want unfilled circles, so you can't use Turtle's
+# "dot" function for this purpose.  Also, you must ensure that
+# lines are not drawn between or connecting the circles.
+#
+# The basic strategy for drawing each circle is to lift
+# up the pen, move to a suitable location on the screen,
+# choose a colour, put the pen down and walk in a circle.
+# Having done this once you can copy your code (with minor
+# changes) three times.
+#
+# Observation: Turtle's "circle" function does NOT draw a
+# circle centred at the current location.  Instead it causes
+# the turtle to walk in a circle, ending up back where
+# it started.
+
+from turtle import *
+
+setup()
+title('Three non-overlapping circles')
+
+## PUT YOUR CODE HERE
+# first circle 
+pendown()
+color('green')
+circle(50)
+# move 
+penup()
+forward(100)
+# sec circle
+pendown()
+color('yellow')
+circle(25)
+
+penup()
+goto(10,100)
+
+pendown()
+color('blue')
+circle(100)
+
+
+
+hideturtle()
+done()
